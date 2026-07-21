f4b50bff5efb93c7c02e25c957d617523350babf
taiatiniyara@gmail <taiatiniyara@gmail.com>
2026-07-21 13:38:03 +1200

    cleanv

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/d1';
-import type { D1Database } from '@cloudflare/workers-types';
-import * as schema from './schema';
-
-export function createDb(database: D1Database) {
-  return drizzle(database, { schema });
-}
-
-export type Db = ReturnType<typeof createDb>;
