16451192d5f6fbd88b2ea989abbeb765a1d6f742
taiatiniyara <taiatiniyara@gmail.com>
2026-08-14 11:07:14 +1200

    clean-slate

--- packages/web/src/lib/observability.ts
+++
@@ -1,9 +0,0 @@
-export {
-  captureError,
-  reportError,
-  reportSyncHealth,
-  addBreadcrumb,
-  type Breadcrumb,
-  type ErrorPayload,
-  type SyncHealthPayload,
-} from '@theobase/observability';
